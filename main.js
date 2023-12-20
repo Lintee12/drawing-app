9d16a8493f6aa185e38e61b9112aa59c1e973e29
Evan Linton <lintonevan@gmail.com>
2023-12-20 19:18:43 -0400

    working version

--- main.js
+++ main.js
@@ -55,6 +55,7 @@ const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 //load values from storage
 window.onload = () => {
     canvas.setAttribute("style", `width:${defaultCanvas[0]}px; height:${defaultCanvas[1]}px;`)
+    gridCanvas.setAttribute("style", `width:${defaultCanvas[0]}px; height:${defaultCanvas[1]}px;`)
     document.getElementById("canvas-size-width").value = defaultCanvas[0];
     document.getElementById("canvas-size-height").value = defaultCanvas[1];
     canvas.width = defaultCanvas[0];
@@ -64,7 +65,7 @@ window.onload = () => {
     drawSizeSlider.value = localStorage.getItem("drawSize");
     drawSizeBind.innerHTML = drawSizeSlider.value;
 
-    if(localStorage.getItem("bgAlphaSize") == null) localStorage.setItem("bgAlphaSize", 18)
+    if(localStorage.getItem("bgAlphaSize") == null) localStorage.setItem("bgAlphaSize", 1)
     bgAlphaSizeSlider.value = localStorage.getItem("bgAlphaSize");
     bgAlphaSizeBind.innerHTML = bgAlphaSizeSlider.value;
 
@@ -88,6 +89,10 @@ window.onload = () => {
     if(localStorage.getItem("brushType") == 1) handleButtonInput(document.getElementById("square-type"));
 
     initCanvas();
+}
+
+const handleHeaderAction = (action) => {
+  console.log(action)
 }
 
 //canvas zoom
@@ -131,6 +136,7 @@ function stopZoom() {
 
 function applyZoom() {
     canvas.style.transform = `scale(${clamp(scaleFactor, 0.1, 100)})`;
+    gridCanvas.style.transform = `scale(${clamp(scaleFactor, 0.1, 100)})`;
 }
 //end canvas zoom
 
@@ -182,37 +188,68 @@ function handleButtonInput(button) {
     }
 }
 
-//validate and handle number inputs
 function handleNumberInput(input) {
-    const value = input.value.trim();
-    const min = parseInt(input.min);
-    const max = parseInt(input.max);
-  
-    if (value === '') return;
-  
-    const numericValue = parseInt(value);
-    if (isNaN(numericValue) || numericValue < min) {
-      input.value = min;
-      input.setAttribute("value", min);
-    } else if (numericValue > max) {
-      input.value = max;
-      input.setAttribute("value", max);
-    } else {
-      input.value = numericValue;
-      input.setAttribute("value", numericValue);
+  const value = input.value.trim();
+  const min = parseInt(input.min);
+  const max = parseInt(input.max);
+
+  if (value === '') return;
+
+  const numericValue = parseInt(value);
+  if (isNaN(numericValue) || numericValue < min) {
+    input.value = min;
+    input.setAttribute("value", min);
+  } else if (numericValue > max) {
+    input.value = max;
+    input.setAttribute("value", max);
+  } else {
+    input.value = numericValue;
+    input.setAttribute("value", numericValue);
+  }
+
+  // Handle canvas size change
+  if (input.dataset.action === "canvas-size") {
+    if (input.id === "canvas-size-width") {
+      canvas.style.width = `${input.value}px`;
+      canvas.width = input.value;
+      gridCanvas.style.width = `${input.value}px`;
+      gridCanvas.width = input.value;
     }
-    //number input events
-    if(input.dataset.action === "canvas-size") {
-      if(input.id === "canvas-size-width") {
-        canvas.setAttribute("style", `width:${input.value}px; height:${canvas.style.height};`);
-        canvas.width = input.value;
-      }
-      if(input.id === "canvas-size-height") {
-        canvas.setAttribute("style", `width:${canvas.style.width}; height:${input.value}px;`);
-        canvas.height = input.value;
-      }
+    if (input.id === "canvas-size-height") {
+      canvas.style.height = `${input.value}px`;
+      canvas.height = input.value;
+      gridCanvas.style.height = `${input.value}px`;
+      gridCanvas.height = input.value;
     }
+
+    // Redraw the grid
+    redrawGrid();
+  }
 }
+
+function redrawGrid() {
+  const ctx = gridCanvas.getContext("2d");
+
+  // Set up the grid pattern
+  const gridSize = 20; // Change this value according to your grid size
+  const numCols = Math.ceil(gridCanvas.width / gridSize);
+  const numRows = Math.ceil(gridCanvas.height / gridSize);
+
+  ctx.clearRect(0, 0, gridCanvas.width, gridCanvas.height);
+  ctx.fillStyle = "#ccc"; // Color for the grid pattern
+  ctx.globalAlpha = 0.2; // Set the transparency for the grid
+
+  for (let col = 0; col < numCols; col++) {
+    for (let row = 0; row < numRows; row++) {
+      if ((col + row) % 2 === 0) {
+        ctx.fillRect(col * gridSize, row * gridSize, gridSize, gridSize);
+      }
+    }
+  }
+
+  ctx.globalAlpha = 1; // Reset the alpha value
+}
+
 
 let colorChangingTimeout;
 
@@ -250,6 +287,8 @@ colorInputs.forEach(input => {
   
 //start canvas logic
 const ctx = canvas.getContext("2d");
+const gridCanvas = document.getElementById('grid-canvas');
+const ctxGrid = gridCanvas.getContext('2d');
 
 let drawnContent = [];
 
@@ -263,7 +302,10 @@ const ratio = devicePixelRatio / backingStoreRatio;
 
 canvas.width = canvas.clientWidth * ratio;
 canvas.height = canvas.clientHeight * ratio;
+gridCanvas.width = canvas.width;
+gridCanvas.height = canvas.height;
 ctx.scale(ratio, ratio);
+ctxGrid.scale(ratio, ratio);
 
 function getMousePos(canvas, event) {
   const rect = canvas.getBoundingClientRect();
@@ -282,41 +324,74 @@ function spawnCircle(locationX, locationY, size) {
   ctx.imageSmoothingEnabled = true;
   ctx.beginPath();
   blobs++;
-  switch (brushType) {
-    case 0:
-      ctx.arc(locationX, locationY, drawSize / 2, 0, Math.PI * 2); //this draws a circle
-      break;
-    case 1:
-      ctx.rect(locationX, locationY, drawSize, drawSize); //this draw a sqare
-    default:
-      break;
+
+  if (!draw) {
+    ctx.globalCompositeOperation = 'destination-out'; // Use destination-out to erase
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)'; // Fill style is irrelevant for erasing
+
+    switch (brushType) {
+      case 0:
+        ctx.arc(locationX, locationY, drawSize / 2, 0, Math.PI * 2);
+        ctx.fill();
+        storeDrawnShape(locationX, locationY, drawSize, ctx.fillStyle, 'erase', true);
+        break;
+      case 1:
+        ctx.fillRect(locationX - drawSize / 2, locationY - drawSize / 2, drawSize, drawSize);
+        storeDrawnShape(locationX, locationY, drawSize, ctx.fillStyle, 'erase', true);
+        break;
+      default:
+        break;
+    }
+    
+    ctx.globalCompositeOperation = 'source-over'; // Reset composite operation for normal drawing
+  } else {
+    ctx.fillStyle = `rgba(${drawColor[0]}, ${drawColor[1]}, ${drawColor[2]}, ${bgAlphaSize})`;
+    
+    switch (brushType) {
+      case 0:
+        ctx.arc(locationX, locationY, drawSize / 2, 0, Math.PI * 2);
+        storeDrawnShape(locationX, locationY, drawSize, ctx.fillStyle, 'circle');
+        break;
+      case 1:
+        ctx.fillRect(locationX - drawSize / 2, locationY - drawSize / 2, drawSize, drawSize);
+        storeDrawnShape(locationX, locationY, drawSize, ctx.fillStyle, 'square');
+        break;
+      default:
+        break;
+    }
+    
+    ctx.fill();
   }
-  storeDrawnShape(locationX, locationY, drawSize, ctx.fillStyle, 'circle');
-  ctx.fill();
 }
 
 function clearCanvas(color, alpha) {
+  // Clear the canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.globalAlpha = alpha; // Set the transparency value
+
+  // Draw the background and the stored non-erased content
+  ctx.globalAlpha = alpha;
   ctx.fillStyle = color;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Redraw the stored content on top of the new background
-  ctx.globalAlpha = 1; // Reset the transparency for the stored content
+  ctx.globalAlpha = 1; // Reset alpha for drawing shapes
+
+  // Redraw only non-erased content on top of the new background
   drawnContent.forEach(shape => {
-    ctx.fillStyle = shape.color;
-    if (shape.type === 'circle') {
-      ctx.beginPath();
-      ctx.arc(shape.x, shape.y, shape.size / 2, 0, Math.PI * 2);
-      ctx.fill();
-    } else if (shape.type === 'square') {
-      ctx.fillRect(shape.x - shape.size / 2, shape.y - shape.size / 2, shape.size, shape.size);
+    if (!shape.isErase) {
+      ctx.fillStyle = shape.color;
+      if (shape.type === 'circle') {
+        ctx.beginPath();
+        ctx.arc(shape.x, shape.y, shape.size / 2, 0, Math.PI * 2);
+        ctx.fill();
+      } else if (shape.type === 'square') {
+        ctx.fillRect(shape.x - shape.size / 2, shape.y - shape.size / 2, shape.size, shape.size);
+      }
     }
   });
 }
 
 const initCanvas = () => {
-  clearCanvas(`rgb(${backgroundColor[0]}, ${backgroundColor[1]}, ${backgroundColor[2]})`, bgAlphaSize);
+  clearCanvas(`rgba(0, 0, 0, 0)`, bgAlphaSize); // Set transparent background
   console.log("Canvas initialized");
 }
 
@@ -331,7 +406,7 @@ function updateDisplay(event) {
     spawnCircle(mousePos.x, mousePos.y, drawSize);
   } 
   else {
-    ctx.fillStyle = `rgb(${backgroundColor[0]}, ${backgroundColor[1]}, ${backgroundColor[2]})`;
+    ctx.fillStyle = `rgba(${backgroundColor[0]}, ${backgroundColor[1]}, ${backgroundColor[2]}, ${bgAlphaSize})`;
     spawnCircle(mousePos.x, mousePos.y, drawSize);
   } 
 }
@@ -358,14 +433,36 @@ canvas.addEventListener("mousemove", (event) => {
   }
 });
 
-function storeDrawnShape(locationX, locationY, size, color, type) {
+function storeDrawnShape(locationX, locationY, size, color, type, isErase) {
   drawnContent.push({
     x: locationX,
     y: locationY,
     size: size,
     color: color,
-    type: type
+    type: type,
+    isErase: isErase // Include an indicator for erasures
   });
 }
+
+// Function to draw the alpha grid
+function drawAlphaGrid() {
+  const gridSize = 20; // Size of each grid square
+  const numCols = Math.ceil(gridCanvas.width / gridSize);
+  const numRows = Math.ceil(gridCanvas.height / gridSize);
+
+  ctxGrid.clearRect(0, 0, gridCanvas.width, gridCanvas.height);
+  ctxGrid.fillStyle = 'rgba(204, 204, 204, 0.2)'; // Color and transparency for the grid pattern
+
+  for (let col = 0; col < numCols; col++) {
+    for (let row = 0; row < numRows; row++) {
+      if ((col + row) % 2 === 0) {
+        ctxGrid.fillRect(col * gridSize, row * gridSize, gridSize, gridSize);
+      }
+    }
+  }
+}
+
+// Call the function to draw the alpha grid
+drawAlphaGrid();
 initCanvas();
 //end canvas logic
